771e95f765733aa0af38bd496b177fd0ac7c9431
Salyz112 <xreyspesal@gmail.com>
2024-12-17 19:12:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,13 +20,6 @@ app.use(function (req, res, next) {
 app.use(express.json());
 
 app.post('/player/login/dashboard', function (req, res) {
-    const tData = {};
-    try {
-        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
-        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
-        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
-    } catch (why) { console.log(`Warning: ${why}`); }
-
     res.render(__dirname + '/public/html/dashboard.html', { data: tData });
 });
 
@@ -45,7 +38,18 @@ app.post('/player/growid/login/validate', (req, res) => {
     );
 });
 
-app.all("/player/growid/checktoken", (req, res, next) => {
+app.get("/player/growid/login/validate", (req, res, next) => {
+const tData = {};
+    try {
+        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
+        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
+        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
+    } catch (why) { console.log(`Warning: ${why}`); }
+     })
+    );
+});
+
+app.post("/player/growid/checktoken", (req, res, next) => {
     const token = req.body.refreshToken;
     if (!token) return res.sendStatus(401);
 
