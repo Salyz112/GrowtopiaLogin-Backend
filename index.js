5fabc477a9eb1d7a648e1f7e2f420b9521de4ade
Salyz112 <xreyspesal@gmail.com>
2024-12-17 18:58:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const rateLimiter = require('express-rate-limit');
+const compression = require('compression');
 
+app.use(compression({
+    level: 5,
+    threshold: 0,
+    filter: (req, res) => {
+        if (req.headers['x-no-compression']) {
+            return false;
+        }
+        return compression.filter(req, res);
+    }
+}));
+app.set('view engine', 'ejs');
+app.set('trust proxy', 1);
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept',
     );
+    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode}`);
     next();
 });
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(function (req, res, next) {
-    console.log(req.method, req.url);
-    next();
-});
-
 app.use(express.json());
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
 
 app.all('/player/login/dashboard', function (req, res) {
     const tData = {};
@@ -27,10 +37,10 @@ app.all('/player/login/dashboard', function (req, res) {
         if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
     } catch (why) { console.log(`Warning: ${why}`); }
 
-    res.render(__dirname + '/public/html/dashboard.html', { data: tData });
+    res.render(__dirname + '/public/html/dashboard.ejs', { data: tData });
 });
 
-app.post('/player/growid/login/validate', (req, res) => {
+app.all('/player/growid/login/validate', (req, res) => {
     // Extracting data from the request body
    const _token = req.body._token;
     const growId = req.body.growId;
@@ -45,7 +55,7 @@ app.post('/player/growid/login/validate', (req, res) => {
     );
 });
 
-app.post("/player/growid/checktoken", (req, res, next) => {
+app.all("/player/growid/checktoken", (req, res, next) => {
     const token = req.body.refreshToken;
     if (!token) return res.sendStatus(401);
 
@@ -59,10 +69,6 @@ app.post("/player/growid/checktoken", (req, res, next) => {
       })
     );
   });
-
-app.post('/player/validate/close', function (req, res) {
-    res.send('<script>window.close();</script>');
-});
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
